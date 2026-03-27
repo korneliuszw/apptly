4dfd19e58f35061c4bb995eb4921c6ebf31ca890
korneliuszw <korneliuszWStudio@gmail.com>
2026-03-27 14:04:25 +0100

    frontend prep 1

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
+import { StrictMode } from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Providers } from "./providers";
+
+// import App from './app/app';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement,
 );
 
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<Providers>
+				<div className="p-2"> test</div>
+			</Providers>
+		</BrowserRouter>
+	</StrictMode>,
 );
