07ea9ea102e74c96508b319bd8bffe75052cf453
korneliuszw <korneliuszWStudio@gmail.com>
2026-03-30 00:17:03 +0200

    fix artifact generation

--- apps/codegen-gateway/src/appService.ts
+++ apps/codegen-gateway/src/appService.ts
@@ -1,17 +1,18 @@
 import simpleGit from "simple-git";
 import { APP_WORKDIR, TEMPLATE_REPOSITORY_URL } from "./constants";
-import { Glob } from "bun";
+
 export const initApp = async () => {
     if (await Bun.file(`${APP_WORKDIR}/.git`).exists()) {
         return "APP_ALREADY_INITIALIZED";
     }
-    await simpleGit().clone(TEMPLATE_REPOSITORY_URL, APP_WORKDIR);
+    await simpleGit().clone(TEMPLATE_REPOSITORY_URL, APP_WORKDIR)
+    await simpleGit(APP_WORKDIR).addConfig("user.name", "Apptly").addConfig("user.email", "bot@apptly.ai")
     return "APP_INITIALIZED";
 }
 
 const createGitArchive = async (inputPath: string, outputPath: string) => {
     const git = simpleGit(inputPath);
-    await git.raw(["archive", "--format=tar-gz", "-o", outputPath, "HEAD"]);
+    await git.raw(["archive", "--format=tar.gz", "-o", outputPath, "HEAD"]);
 }
 
 export const createAppArtifact = async () => {
